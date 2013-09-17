5679d3de2886df4636147d3da6ac7b5f8a052575
Hissao <andre.hissao@outlook.com>
2013-09-17 16:39:23 -0300

    Setting products to root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Me::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'products#index'
 
 end
