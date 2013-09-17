79e48646f63fc1ce577162ebb92c83c5f826fedc
Hissao <andre.hissao@outlook.com>
2013-09-17 13:27:47 -0300

    Show image when editing product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :image_remote_url, :code, :description, :name, :price
+  attr_accessible :image_remote_url, :code, :description, :name, :price, :image
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
   validates :name, :code, :presence => true
